4539c8e40c3af07e8894ba7e28fa60d1bf74103e
Hari Mohan Meena <hari251097@gmail.com>
2020-12-27 22:37:18 +0530

    Add files via upload

---
+++ 04-11-2020/sql/Assignment 7/gin with mysql.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Product struct {
+	Productid   string `json:"Productid,omitempty"`
+	Productname string `json:"Productname,omitempty"`
+	Producttype string `json:"Producttype,omitempty"`
+	Productdesc string `json:"Productdesc,omitempty"`
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/product", GetProductEndpoint)
+	r.GET("/product/:id", GetProduct1Endpoint)
+	r.POST("/product", CreateProductEndpoint)
+	r.PUT("/product/:id", PutProductEndpoint)
+	r.DELETE("/product/:id", DeleteProductEndpoint)
+	r.Run(":123")
+}
+
+func DeleteProductEndpoint(c *gin.Context) {
+	id := c.Params.ByName("id")
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/hello")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	result, err := db.Query("delete from product where Productid=?", id)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+
+	var product []Product
+	result1, err := db.Query("SELECT * from product")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result1.Close()
+
+	for result1.Next() {
+		var a1 Product
+		err := result1.Scan(&a1.Productid, &a1.Productname, &a1.Producttype, &a1.Productdesc)
+		if err != nil {
+			panic(err.Error())
+		}
+		product = append(product, a1)
+	}
+	c.JSON(200, product)
+}
+
+func PutProductEndpoint(c *gin.Context) {
+	id := c.Params.ByName("id")
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/hello")
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	k := "update product set Productname='" + keyVal["Productname"] + "',Producttype='" + keyVal["Producttype"] + "',Productdesc='" + keyVal["Productdesc"] + "' where Productid=" + id
+	//{"Productname":"hari123","Producttype":"person123","Productdesc":"Hari Mohan"}
+	result, err := db.Query(k)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	var product []Product
+	result1, err := db.Query("SELECT * from product")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result1.Close()
+
+	for result1.Next() {
+		var a1 Product
+		err := result1.Scan(&a1.Productid, &a1.Productname, &a1.Producttype, &a1.Productdesc)
+		if err != nil {
+			panic(err.Error())
+		}
+		product = append(product, a1)
+	}
+	c.JSON(200, product)
+}
+
+func CreateProductEndpoint(c *gin.Context) {
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/hello")
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	k := "insert into product values(" + keyVal["Productid"] + ",'" + keyVal["Productname"] + "','" + keyVal["Producttype"] + "','" + keyVal["Productdesc"] + "')"
+	//{"Productname":"hari","Productid":"3","Producttype":"person","Productdesc":"Hari Mohan"}
+	result, err := db.Query(k)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+
+	var product []Product
+	result1, err := db.Query("SELECT * from product")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result1.Close()
+
+	for result1.Next() {
+		var a1 Product
+		err := result1.Scan(&a1.Productid, &a1.Productname, &a1.Producttype, &a1.Productdesc)
+		if err != nil {
+			panic(err.Error())
+		}
+		product = append(product, a1)
+	}
+	c.JSON(200, product)
+}
+
+func GetProduct1Endpoint(c *gin.Context) {
+	id := c.Params.ByName("id")
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/hello")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	result, err := db.Query("SELECT * from product WHERE Productid = ?", id)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+
+	var a1 Product
+	for result.Next() {
+		err := result.Scan(&a1.Productid, &a1.Productname, &a1.Producttype, &a1.Productdesc)
+		if err != nil {
+			panic(err.Error())
+		}
+
+	}
+	c.JSON(200, a1)
+}
+
+func GetProductEndpoint(c *gin.Context) {
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/hello")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	var product []Product
+	result, err := db.Query("SELECT * from product")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+
+	for result.Next() {
+		var a1 Product
+		err := result.Scan(&a1.Productid, &a1.Productname, &a1.Producttype, &a1.Productdesc)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		product = append(product, a1)
+	}
+	fmt.Print(product)
+	c.JSON(200, product)
+}
---
+++ 04-11-2020/sql/Assignment 7/gorilla with mysql.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	_ "io/ioutil"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+type Product struct {
+	Productid   string `json:"Productid,omitempty"`
+	Productname string `json:"Productname,omitempty"`
+	Producttype string `json:"Producttype,omitempty"`
+	Productdesc string `json:"Productdesc,omitempty"`
+}
+
+func GetProductEndpoint(w http.ResponseWriter, req *http.Request) {
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/hello")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	var product []Product
+	result, err := db.Query("SELECT * from product")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+
+	for result.Next() {
+		var a1 Product
+		err := result.Scan(&a1.Productid, &a1.Productname, &a1.Producttype, &a1.Productdesc)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		product = append(product, a1)
+	}
+	fmt.Print(product)
+	json.NewEncoder(w).Encode(product)
+}
+
+func GetProduct1Endpoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/hello")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	result, err := db.Query("SELECT * from product WHERE Productid = ?", params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+
+	var a1 Product
+	for result.Next() {
+		err := result.Scan(&a1.Productid, &a1.Productname, &a1.Producttype, &a1.Productdesc)
+		if err != nil {
+			panic(err.Error())
+		}
+
+	}
+	json.NewEncoder(w).Encode(a1)
+}
+
+func CreateProductEndpoint(w http.ResponseWriter, req *http.Request) {
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/hello")
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	k := "insert into product values(" + keyVal["Productid"] + ",'" + keyVal["Productname"] + "','" + keyVal["Producttype"] + "','" + keyVal["Productdesc"] + "')"
+	//{"Productname":"hari","Productid":"3","Producttype":"person","Productdesc":"Hari Mohan"}
+	result, err := db.Query(k)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	fmt.Fprintf(w, "New post was created")
+
+	var product []Product
+	result1, err := db.Query("SELECT * from product")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result1.Close()
+
+	for result1.Next() {
+		var a1 Product
+		err := result1.Scan(&a1.Productid, &a1.Productname, &a1.Producttype, &a1.Productdesc)
+		if err != nil {
+			panic(err.Error())
+		}
+		product = append(product, a1)
+	}
+	json.NewEncoder(w).Encode(product)
+}
+
+func DeleteProductEndpoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/hello")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	result, err := db.Query("delete from product where Productid=?", params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+
+	fmt.Fprintf(w, "post is deleted")
+
+	var product []Product
+	result1, err := db.Query("SELECT * from product")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result1.Close()
+
+	for result1.Next() {
+		var a1 Product
+		err := result1.Scan(&a1.Productid, &a1.Productname, &a1.Producttype, &a1.Productdesc)
+		if err != nil {
+			panic(err.Error())
+		}
+		product = append(product, a1)
+	}
+	json.NewEncoder(w).Encode(product)
+}
+
+func PutProductEndpoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/hello")
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	k := "update product set Productname='" + keyVal["Productname"] + "',Producttype='" + keyVal["Producttype"] + "',Productdesc='" + keyVal["Productdesc"] + "' where Productid=" + params["id"]
+	//{"Productname":"hari123","Producttype":"person123","Productdesc":"Hari Mohan"}
+	result, err := db.Query(k)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	fmt.Fprintf(w, " post was updated")
+
+	var product []Product
+	result1, err := db.Query("SELECT * from product")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result1.Close()
+
+	for result1.Next() {
+		var a1 Product
+		err := result1.Scan(&a1.Productid, &a1.Productname, &a1.Producttype, &a1.Productdesc)
+		if err != nil {
+			panic(err.Error())
+		}
+		product = append(product, a1)
+	}
+	json.NewEncoder(w).Encode(product)
+}
+
+func main() {
+
+	/*insert, err := db.Query("create table product(Productid integer,Productname varchar(30),Producttype varchar(20),Productdesc varchar(300))")
+	insert, err := db.Query(" insert into product values(0,'machine','tech','machine learning')")
+	insert, err := db.Query(" insert into product values(1,'welder','machnical','gas welder')")
+	*/
+	router := mux.NewRouter()
+	router.HandleFunc("/product", GetProductEndpoint).Methods("GET")
+	router.HandleFunc("/product/{id}", GetProduct1Endpoint).Methods("GET")
+	router.HandleFunc("/product/{id}", PutProductEndpoint).Methods("PUT")
+	router.HandleFunc("/product", CreateProductEndpoint).Methods("POST")
+	router.HandleFunc("/product/{id}", DeleteProductEndpoint).Methods("DELETE")
+	http.ListenAndServe(":123", router)
+}
